755ba2603749056d335c1162a39bd57d08232a9e
arafik <memedplay@gmail.com>
2024-04-02 17:08:14 +0200

    manage multiple env

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDq3Ltmyd1xLbYLXK2NtESsGO2HgmAOztI",
-    authDomain: "mesptitsplats-7c3eb.firebaseapp.com",
-    projectId: "mesptitsplats-7c3eb",
-    storageBucket: "mesptitsplats-7c3eb.appspot.com",
-    messagingSenderId: "979698512218",
-    appId: "1:979698512218:web:e86ab1a4c8edffc3a28363"
+    apiKey: `${process.env.API_KEY}`,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
+    projectId: `${process.env.PROJECT_ID}`,
+    storageBucket: `${process.env.STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+    appId: `${process.env.APP_ID}`
 };
 
 // Initialize Firebase
